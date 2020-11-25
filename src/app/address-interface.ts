c5aa2b8d2a39a566122ed7e65a5a62ef2797b71f
ike-njoku <ikenjokudc@gmail.com>
2020-11-25 07:54:01 -0800

    added address interface

---
+++ src/app/address-interface.ts
@@ -0,0 +1,13 @@
+export interface address{
+    firstName?: string;
+    lastName?: string;
+    zip?: string;
+    email?: string;
+    phoneNumber?: number;
+    altPhoneNumber?: number;
+    street?: string;
+    privince?: string; 
+    city?: string;
+    state?: string;
+    country?: string;
+}
