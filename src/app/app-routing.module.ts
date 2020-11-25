fe642632dad283c46e19b02757c0b8b071351fc0
ike-njoku <ikenjokudc@gmail.com>
2020-11-25 10:12:23 -0800

    updated app-routing-module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/route-list', pathMatch: 'full'},
+  {path: 'route-list', component:ListComponent},
+  // wild card
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
