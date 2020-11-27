14129de7da5696873be9e0687e93b888724108ad
ike-njoku <ikenjokudc@gmail.com>
2020-11-26 22:55:55 -0800

    added pick-up service (angular cli)

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule} from '@angular/material/toolbar'
+import { MatTableModule } from  '@angular/material/table'
 
 const MaterialComponents = [
   MatButtonModule,
   MatToolbarModule,
+  MatTableModule
+
 ]
 
 @NgModule({
