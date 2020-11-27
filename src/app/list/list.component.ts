b120811cd67a2f9030284fca76da3021ada69851
ike-njoku <ikenjokudc@gmail.com>
2020-11-27 09:52:03 -0800

    created table for list view to hold data

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,13 +3,25 @@ import { PickUp } from '../pick-up-interface';
 
 
 
-// DATA SOURCE
+// DATA SOURCE (replace using http client and a url)
 const TABLE_DATA: PickUp[] = [
 
-  {id: 1, pickUpTime:'Wed Nov 25 2020 11:23:26 GMT-0800', address: {
+  {id: 1, 
+    pickUpTime:'Wed Nov 25 2020 11:23:26 GMT-0800', 
+    address: {
     firstName:'David',
+    phoneNumber: 7038686694,
     zip:'5445-Aba',
     street:'MungoPark'
+  }},
+  
+  {id: 2, 
+    pickUpTime:'Wed Nov 25 2020 12:54:47 GMT-0800', 
+    address: {
+    firstName:'Chidalu',
+    phoneNumber: 7038792802,
+    zip:'4127-OWR',
+    street:'Chukwuma Nworah'
   }}
 
 ];
@@ -21,6 +33,11 @@ const TABLE_DATA: PickUp[] = [
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  // the data source from where the table is populated
+  dataSource = TABLE_DATA;
+
+  // defining the columns to be displayed
+  displayedColumns: string[]=['id', 'clientName', 'pickUpLocation', 'time'];
 
   constructor() { }
   
